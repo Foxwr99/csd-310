670b44c163bc01f194be616deec541b230467ba9
Wendy Rodriguez <89361562+Foxwr99@users.noreply.github.com>
2022-06-16 00:33:09 -0700

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-url = "mongodb+srv://admin:admin@cluster0.q1zeowl.mongodb.net/pytech"
-client = MongoClient(mongodb+srv://admin:adming@cluster0.q1zeowl.mongodb.net/pytech)
+url = "mongodb+srv://admin:admin@cluster0.q1zeowl.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient("mongodb+srv://admin:admin@cluster0.q1zeowl.mongodb.net/?retryWrites=true&w=majority")
 db = client.pytech
 print(db.list_collection_names())
