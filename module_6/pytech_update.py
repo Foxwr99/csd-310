dcbd60e3513ed20582e2c9df765dcfd981f80711
Wendy Rodriguez <foxwr99@gmail.com>
2022-06-23 21:36:17 -0700

    update file with author name

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,3 +1,8 @@
+"""Wendy Rodriguez
+June 23, 2022
+Module 6.2 Assignment"""
+
+
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.q1zeowl.mongodb.net/?retryWrites=true&w=majority"
