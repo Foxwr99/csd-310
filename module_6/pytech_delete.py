0f7aad5365e49caa95d39333ba939d981bf0e7fc
Wendy Rodriguez <foxwr99@gmail.com>
2022-06-23 22:55:12 -0700

    added author name

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -1,3 +1,7 @@
+"""Wendy Rodriguez
+June 23, 2022
+Module 6.3 Assignment"""
+
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.q1zeowl.mongodb.net/?retryWrites=true&w=majority"
