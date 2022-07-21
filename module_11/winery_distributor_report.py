d22b6d096f001907a4944416be08c2df3063efe4
Wendy Rodriguez <foxwr99@gmail.com>
2022-07-21 13:14:27 -0700

    Added new report of how many barrels sold

--- module_11/winery_distributor_report.py
+++ module_11/winery_distributor_report.py
@@ -18,14 +18,17 @@ cursor.execute('SELECT name, wine FROM distributors')
 
 distributors = cursor.fetchall()
 print("--- LIST OF DISTRIBUTORS AND THE WINE THEY CARRY ---")
+print("--- DATE OF REPORT: JULY 2022 ---")
 for distributor in distributors:
     print("\nDistributor Name: {}".format(distributor[0]), "\nWine: {}".format(distributor[1]))
 print()
 print("Press any key to continue...")
 print()
+
 # Create a report showing how many distributors are selling each wine
 
 print("--- LIST OF HOW MANY DISTRIBUTORS CARRY EACH WINE ---")
+print("--- DATE OF REPORT: JULY 2022 ---")
 cursor.execute('SELECT wine, COUNT(wine) AS num FROM distributors GROUP BY wine HAVING COUNT(wine) > 0')
 wines = cursor.fetchall()
 for wine in wines:
@@ -33,3 +36,16 @@ for wine in wines:
 print()
 print("Press any key to continue...")
     
+# Create a report of how many barrels of each wine was sold
+
+print()
+print("--- EXPECTED SALES (IN BARRELS) VS ACTUAL SALES ---")
+print("--- DATE OF REPORT: JULY 2022 ---")
+cursor.execute('SELECT wine, SUM(expected_sales) as num, SUM(actual_sales) from distributors GROUP by wine HAVING COUNT(expected_sales) > 0')
+sales = cursor.fetchall()
+for sale in sales:
+    print("\nWine: {}".format(sale[0]), "\nExpected Sales: {}".format(sale[1]), "\nActual Sales: {}".format(sale[2]))
+print()
+print("Press any key to continue...")
+
+db.close()
